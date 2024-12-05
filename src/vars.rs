bf42cb6f514c88c3f670e329c5ea8f77c95a1074
Dinno <tahadlrb7@gmail.com>
2024-12-05 21:55:04 +0330

    play with variables

---
+++ src/vars.rs
@@ -0,0 +1,12 @@
+pub fn run(){
+    let mut name = "Taha";
+    println!("{}", name);
+    name = "Dinno";
+    println!("{}", name);
+
+    let mut age = 23;
+    println!("{}", age);
+    age += 1;
+    println!("{}", age);
+
+}
